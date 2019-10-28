1f78086a6fcc17654ad51618e436310239ae268b
Bruno Parga <brunoparga@gmail.com>
2019-10-28 15:49:42 -0300

    Authenticate all feed routes

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -50,7 +50,11 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch(`http://localhost:8080/feed/posts?page=${page}`)
+    fetch(`http://localhost:8080/feed/posts?page=${page}`, {
+      headers: {
+        Authorization: `Bearer ${this.props.token}`,
+      },
+    })
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch posts.');
@@ -118,7 +122,13 @@ class Feed extends Component {
       method = 'PUT';
     }
 
-    fetch(url, { method, body: formData })
+    fetch(url, {
+      method,
+      body: formData,
+      headers: {
+        Authorization: `Bearer ${this.props.token}`
+      },
+    })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error('Creating or editing a post failed!');
@@ -168,7 +178,12 @@ class Feed extends Component {
 
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
-    fetch(`http://localhost:8080/feed/post/${postId}`, { method: 'DELETE'})
+    fetch(`http://localhost:8080/feed/post/${postId}`, {
+      method: 'DELETE',
+      headers: {
+        Authorization: `Bearer ${this.props.token}`
+      },
+    })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error('Deleting a post failed!');
