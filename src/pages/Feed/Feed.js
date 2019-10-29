210f979b66eae977ab0f10dc36ef967336a16738
Bruno Parga <brunoparga@gmail.com>
2019-10-29 16:36:02 -0300

    Delete posts via socket

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -44,6 +44,8 @@ class Feed extends Component {
         this.addPost(data.post)
       } else if (data.action === 'update') {
         this.updatePost(data.post)
+      } else if (data.action === 'delete') {
+        this.loadPosts();
       }
     })
   }
@@ -216,14 +218,7 @@ class Feed extends Component {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error('Deleting a post failed!');
         }
-        return res.json();
-      })
-      .then(resData => {
-        console.log(resData);
-        this.setState(prevState => {
-          const updatedPosts = prevState.posts.filter(p => p._id !== postId);
-          return { posts: updatedPosts, postsLoading: false };
-        });
+        this.loadPosts();
       })
       .catch(err => {
         console.log(err);
